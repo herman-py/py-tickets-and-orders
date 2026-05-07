a6989df4e306ce4316dcc0699752604cb968f898
Dmytro Herman <279219406+herman-py@users.noreply.github.com>
2026-05-07 18:38:41 +0300

    Services

---
+++ services/order.py
@@ -0,0 +1,35 @@
+from django.db.models import QuerySet
+from django.db import transaction
+
+from db.models import Order, Ticket
+from services.user import get_user_model
+
+
+@transaction.atomic
+def create_order(
+    tickets: list[dict],
+    username: str,
+    date: str = None,
+) -> Order:
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+
+    if date:
+        order.created_at = date
+        order.save()
+
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+            movie_session_id=ticket_data["movie_session"],
+        )
+
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(user__username=username)
+    return Order.objects.all()
